eb4c4b02d3303a75479c410aa4111b221d63c763
seongha <11pi885@gmail.com>
2024-01-03 21:17:36 +0900

    chore : 초기 셋팅

---
+++ src/main/java/com/example/crosspuzzleserver/CrossPuzzleServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.crosspuzzleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossPuzzleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossPuzzleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crosspuzzleserver/CrossPuzzleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crosspuzzleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossPuzzleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
