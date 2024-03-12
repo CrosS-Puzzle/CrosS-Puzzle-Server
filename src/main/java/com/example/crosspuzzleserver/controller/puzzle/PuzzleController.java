dbdc19bb95eed28d9f29ca32d7e05bd52474e148
seongha <11pi885@gmail.com>
2024-03-12 14:33:36 +0900

    fix : cors 허용

--- src/main/java/com/example/crosspuzzleserver/controller/puzzle/PuzzleController.java
+++ src/main/java/com/example/crosspuzzleserver/controller/puzzle/PuzzleController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 @RequestMapping("")
 public class PuzzleController {
 
